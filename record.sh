f2f78ec14da06500a75fac66cab4de8c5f60ae51
Peter Körner <pkoerner@seibert-media.net>
2017-09-27 12:34:40 +0200

    use timestamp instead of wonky formatted date-time

--- record.sh
+++ record.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-raspistill --verbose --hflip --vflip -ss 4000 --awb off --awbgains 1.5,1.3 --exposure off --ISO 100 --metering matrix --ev -24 -o /opt/timelapse/images/lapse-%d.jpg --datetime --timelapse 10000 --timeout 0
+raspistill --verbose --hflip --vflip -ss 4000 --awb off --awbgains 1.5,1.3 --exposure off --ISO 100 --metering matrix --ev -24 -o /opt/timelapse/images/lapse-%d.jpg --timestamp --timelapse 10000 --timeout 0
